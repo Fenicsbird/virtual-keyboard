5ffd2d5579545de09df6674de1cd483d0d904f7a
Natalya Kalinicheva <natalya92281979@gmail.com>
2023-04-28 18:57:48 +0300

    feat: add keyboard

--- script.js
+++ script.js
@@ -2,10 +2,41 @@
 let titleDiv = document.createElement('div');
 titleDiv.className = "title";
 titleDiv.innerHTML = "RSS Virtual Keyboard";
-document.body.append(titleDiv);
+document.body.appendChild(titleDiv);
 
 // input window
 let inputDiv = document.createElement('div');
 inputDiv.className = "input";
-document.body.append(inputDiv);
+document.body.appendChild(inputDiv);
+
+// keyboard
+function createVirtualKeyboard() {
+// Create a div to hold the virtual keyboard
+  const keyboardDiv = document.createElement('div');
+  keyboardDiv.className = "wrapper";
+// Define the keys on the virtual keyboard
+  const keys = [
+    "`", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=", "Back",
+    "Tab", "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "[", "]", "/", "Del",
+    "Caps", "a", "s", "d", "f", "g", "h", "j", "k", "l", ";", "'", "Enter",
+    "Shift", "z", "x", "c", "v", "b", "n", "m", ",", ".", "/", " ", "Shift",
+    "Ctrl", "Win", "Alt", "  ", "Alt", " ", " ", " ", "Ctrl"
+    ];
+// Loop through the keys and create a button for each one
+  for (let i = 0; i < keys.length; i++) {
+    const button = document.createElement("button");
+    button.className = "keyboard__btn"
+    button.textContent = keys[i];
+
+    button.addEventListener("click", () => {
+// Update the input field with the clicked key
+    const inputField = document.querySelector("input");
+    inputField.value += keys[i];
+    });
+    keyboardDiv.appendChild(button);
+  }
+// Add the virtual keyboard to the document
+    document.body.appendChild(keyboardDiv);
+}
+createVirtualKeyboard();
 
